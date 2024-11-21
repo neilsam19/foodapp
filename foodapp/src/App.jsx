9ffb415352b59108492c12331a08bdb759d45633
Neil Samant <nsamant8@gatech.edu>
2024-11-20 19:08:18 -0500

    Added Inner and Outer Containers to maintain structural integrity

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 import Search from "./components/Search";
 import FoodList from "./components/FoodList";
 import Nav from "./components/Nav";
+import Container from "./components/Container";
 import "./App.css";
+import InnerContainer from "./components/InnerContainer";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
@@ -10,7 +12,11 @@ function App() {
     <div className="App">
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
-      <FoodList foodData={foodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} />
+        </InnerContainer>
+      </Container>
     </div>
   );
 }
